ea69a57d4c6ac7bb6c61fda0d503cccad297fb47
Eric Waller <erwaller@gmail.com>
2012-09-08 15:35:32 -0400

    slightly fancier js

--- partysupply/static/js/app.js
+++ partysupply/static/js/app.js
@@ -4,10 +4,18 @@ var InstagramPostView = Backbone.View.extend({
   template: _.template($("#instagram-post-view").html()),
 
   render: function () {
+    var that = this;
     this.$el.html(this.template({
       media: this.options.media,
       isodate: (new Date(parseInt(this.options.media.created_time, 10) * 1000)).toISOString()
     }));
+    this.$el.hide();
+    this.$(".image")
+      .load(function () {
+        _.defer(function () { that.$el.fadeIn(); });
+        that.trigger("ready");
+      })
+      .attr("src", this.options.media.images.standard_resolution.url);
     this.$(".timestamp").timeago();
     return this;
   }
@@ -15,10 +23,11 @@ var InstagramPostView = Backbone.View.extend({
 });
 
 $(function () {
-  
+  var count = 0;
+
   _.each(BOOTSTRAP_DATA, function (media) {
     var view = new InstagramPostView({ media: media });
-    view.render().$el.appendTo("#wrap");
+    setTimeout(function () { view.render().$el.prependTo(".posts"); }, (++count) * 350);
   });
   
 });
