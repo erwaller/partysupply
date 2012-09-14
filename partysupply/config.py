0903d0b23fe0e96d66d813e5168ee093eb0aea67
Eric Waller <erwaller@gmail.com>
2012-09-13 22:39:04 -0400

    a few more tweaks

--- partysupply/config.py
+++ partysupply/config.py
@@ -1,8 +1,8 @@
 import os
 
-SG_ENV = os.environ.get('SG_ENV', 'dev')
+SG_ENV = os.environ.get("SG_ENV", "dev")
 DEBUG = (SG_ENV == "dev")
-BASE_URL = "https://nda2.showoff.io"
+BASE_URL = os.environ.get("BASE_URL")
 
 INSTAGRAM_CLIENT_ID = os.environ["INSTAGRAM_CLIENT_ID"]
 INSTAGRAM_CLIENT_SECRET = os.environ["INSTAGRAM_CLIENT_SECRET"]
